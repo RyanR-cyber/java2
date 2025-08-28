4c20f67867dc27184d1641c4f5312f582b6e4dd5
RyanR-cyber <ozzy19992@gmail.com>
2025-08-27 20:14:49 -0500

    initlize

---
+++ app.js
@@ -0,0 +1,18 @@
+let video = "Youtube";
+let hobby = "coding";
+console.log(`My favorite hobby is ${hobby}.`);
+console.log(`I love watching ${video} videos!`);
+{
+    let x = 10;
+} // variable is block 
+
+var y = 20; // variable is open
+let text = "Hello, World!"; // this is a string variable
+console.log(text); 
+let a = 5; // this is a number variable
+let b = 10; // this is another number variable  
+let sum = a + b; // this is the sum of a and b
+console.log(sum); // output the sum to the console
+const pc = {type:"laptop", brand:"Dell", color:"black"}; // this is an object variable using const
+pc.color = "blue"; // changing the color property of the pc object
+console.log(pc); // output the pc object to the console
